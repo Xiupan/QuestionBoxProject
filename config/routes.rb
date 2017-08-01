1d24ce8d6c6264e860d240ddabff87597cb7ca93
Alan Hong <xiupan@gmail.com>
2017-08-01 11:33:03 -0500

    User registration, login, and logout works!

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :questions
   resources :users
+  get 'sessions/new' => 'sessions#new', as: :login
+  post 'sessions/new' => 'sessions#create'
+  delete 'sessions' => 'sessions#destroy', as: :logout
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
